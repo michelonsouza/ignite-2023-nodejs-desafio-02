23e4123cc3a103bab85b04b65751457f4697aa56
Michelon Souza <michelon.souza@hotmail.com>
2023-08-01 06:09:53 -0300

    feat(migrations): create users and snakcs migrations

---
+++ db/migrations/20230801085926_create-snacks.ts
@@ -0,0 +1,18 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('snacks', table => {
+    table.uuid('id').primary();
+    table.uuid('user_id').notNullable();
+    table.foreign('user_id').references('users.id').deferrable('deferred');
+    table.text('name').notNullable();
+    table.text('description').notNullable();
+    table.boolean('is_on_the_diet').notNullable().defaultTo(true);
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).nullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('snacks');
+}
--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -2,13 +2,13 @@
 import { Knex } from 'knex';
 
 declare module 'knex/types/tables' {
+  interface User {
+    id: string;
+    name: string;
+    created_at: string;
+  }
+
   export interface Tables {
-    transactions: {
-      id: string;
-      title: string;
-      amount: number;
-      created_at: string;
-      session_id?: string;
-    };
+    users: User;
   }
 }
